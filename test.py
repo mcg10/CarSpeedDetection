0621e2eed96e6dea7fc9a535ede3585e31f150d2
Matthew Giglio <matthew.giglio@duke.edu>
2023-04-04 20:49:12 -0400

    Added visual test

---
+++ test.py
@@ -0,0 +1,25 @@
+import pafy
+import cv2
+
+url = "https://www.youtube.com/watch?v=5_XSYlAfJZM"  # Tilton
+
+
+def resize_frame(frame):
+    scale_percent = 60
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+    return resized
+
+
+if __name__ == '__main__':
+    url = "https://www.youtube.com/watch?v=5_XSYlAfJZM"  # Tilton
+    video = pafy.new(url)
+    best = video.getbest(preftype="mp4")
+    capture = cv2.VideoCapture(best.url)
+    while True:
+        _, frame = capture.read()
+        frame = resize_frame(frame)
+        cv2.imshow('frame', frame)
+        cv2.waitKey(1)
