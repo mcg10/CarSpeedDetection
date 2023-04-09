7d84aca587d62201d3a58f525b08c1fd088546c4
Matthew Giglio <matthew.giglio@duke.edu>
2023-04-08 21:18:04 -0400

    trying imageio

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pafy
 import cv2
 from ffpyplayer.player import MediaPlayer
 import numpy as np
+import imageio.v3 as iio
 
 url = "https://www.youtube.com/watch?v=5_XSYlAfJZM"  # Tilton
 
@@ -16,16 +17,6 @@ def resize_frame(frame):
 
 
 if __name__ == '__main__':
-    url = "https://www.youtube.com/watch?v=5_XSYlAfJZM"  # Tilton
-    video = pafy.new(url)
-    best = video.getbest(preftype="mp4")
-    player = MediaPlayer("test_video.mp4")
-    while True:
-        frame, _ = player.get_frame()
-        if frame:
-            img, t = frame
-            w = img.get_size()[0]
-            h = img.get_size()[1]
-            arr = np.uint8(np.asarray(list(img.to_bytearray()[0])).reshape(h, w, 3))
-            cv2.imshow('frame', arr)
-            cv2.waitKey(1)
+    for frame in iio.imiter("test_video.mp4", plugin="pyav"):
+        cv2.imshow('frame', frame)
+        cv2.waitKey(1)
